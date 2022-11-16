c48d01b3c08f76b2041981b1cd275c2fa25d38c7
SlobodianiukDS <107937529+SlobodianiukDS@users.noreply.github.com>
2022-11-16 23:07:49 +0200

    2-3 задание

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,20 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+const listEl = document.querySelector(`#ingredients`);
+
+const elements = ingredients.map((element) => {
+  const newElements = document.createElement(`li`);
+  newElements.classList = `item`;
+  newElements.textContent = element;
+
+  return newElements;
+});
+
+listEl.append(...elements);
